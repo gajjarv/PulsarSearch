259a787266cfa1d0d5ea12731150aa5e5622e03d
gajjarv <vishalg@berkeley.edu>
2018-10-14 01:05:18 -0700

    New repo

--- versions/PlotCand_v2.py
+++ versions/PlotCand_v2.py
@@ -10,40 +10,7 @@ import glob
 from itertools import chain
 from os.path import basename
 from itertools import tee, izip
-import matplotlib.pyplot as plt
-import psrchive as psr
-#from sigpyproc.Readers import FilReader
-
-def find_nearidx(array,val):
-        idx = (np.abs(array-val)).argmin()
-        return idx
-
-def dedispblock(ar,lodm,hidm):
-    fpsr = psr.Archive_load(ar)
-    toplot = []
-    dmstep = 1
-    dmrange = range(lodm,hidm,dmstep)
-    for dm in dmrange:
-        fpsr.remove_baseline()
-        fpsr.set_dispersion_measure(dm)
-        fpsr.dedisperse()
-        ds = fpsr.get_data().squeeze()
-        w = fpsr.get_weights().flatten()
-        w = w/np.max(w) # Normalized it
-        idx = np.where(w==0)[0]
-        ds = np.multiply(ds, w[np.newaxis,:,np.newaxis]) # Apply it
-        ds[:,idx,:] = np.nan
-        data1 = ds[0,:,:]
-        time = np.nanmean(data1[:,:],axis=0)
-        toplot.append(time)
-
-    tbin = float(fpsr.integration_length()/fpsr.get_nbin())        
-    taxis = np.arange(0,fpsr.integration_length(),tbin)
-    taxis=taxis*1000 #Get to msec
-    toplot = np.array(toplot)
-    toplot = [list(i) for i in zip(*toplot)]
-    toplot = np.transpose(toplot)
-    return toplot,taxis
+from sigpyproc.Readers import FilReader
 
 def pairwise(iterable):
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
@@ -116,7 +83,7 @@ def extractPlotCand(fil_file,frb_cands,noplot,fl,fh,tint,Ttot,kill_time_range,ki
                         for indx,frb in enumerate(frb_cands):
                                 time = frb['time']
                                 dm = frb['dm']
-	    			filter = frb['filter']
+				filter = frb['filter']
 				width = tint * (2 ** filter)*(10**3) # Width in msec
 				snr = frb['snr']
 
@@ -161,16 +128,6 @@ def extractPlotCand(fil_file,frb_cands,noplot,fl,fh,tint,Ttot,kill_time_range,ki
 						# Correct the variable baseline, this script writes out .norm files 	
 						cmd = "running_mean_sub %s.ar" % (candname)
 						os.system(cmd)
-                                                
-                                                ar = candname + ".norm"
-
-                                
-                                                lodm = int(dm-(dm*0.15))
-                                                if lodm < 0: lodm = 0
-                                                hidm = int(dm+(dm*0.15))
-                                                print "Producing DM vs phase plot"
-                                                toplot,taxis = dedispblock(ar,lodm,hidm)
-                                                #plt.show()
 
                                                 for i,j in pairwise(frac):
                                                     #New plotting technique
@@ -202,28 +159,7 @@ def extractPlotCand(fil_file,frb_cands,noplot,fl,fh,tint,Ttot,kill_time_range,ki
                                                     print cmd
                                                     os.system(cmd)
 
-                                                    #DM vs time plot 
-                                                    plt.rcParams["figure.figsize"] = (20,8)
-                                                    plt.rcParams.update({'font.size':22})
-                                                    plt.set_cmap('gray')
-                                                    plt.xlabel("Time (msec)")
-                                                    plt.ylabel("DM")
-                                                    lti = int(np.floor(len(taxis)*i))
-                                                    lhi = int(np.ceil(len(taxis)*j))
-                                                    if(lhi>=len(taxis)): lhi = len(taxis)-1
-                                                    lt = taxis[lti]
-                                                    lh = taxis[lhi]
-                                                    #plt.xlim(lti,lhi)
-                                                    plt.imshow(toplot[:,lti:lhi],extent=[lt,lh,lodm,hidm],origin='lower',aspect='auto')
-                                                    #plt.imshow(toplot,origin='lower',aspect='auto')
-                                                    png = candname + ".dmspace_%.2f.ps" % (i)
-                                                    plt.savefig(png) 
-                                                    
-                                                    cmd = "convert -rotate 90 %s_%.2f.ps +append \( -trim -resize 560x700 %s +append \) -append %s_%.2f.pdf" % (candname,i,png,candname,i)
-                                                    os.system(cmd)
-
-
-		        cmd = "gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER -sOutputFile=%s_frb_cand.pdf *sec*DM*.pdf" % (source_name)	  
+		        cmd = "gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER -sOutputFile=%s_frb_cand.pdf *sec*DM*.ps" % (source_name)	  
                         print cmd
                         os.system(cmd)
                 else:
@@ -245,12 +181,11 @@ if __name__ == "__main__":
     fh = 1500
     noplot=0
     tint=0.000163
-    Ttot = 80 # Total length of the file        
+    Ttot = 20 # Total length of the file        
     kill_time_range=[]
     kill_chans=[]
-    nchan = 2048
     source_name="Fake"
-    
+
     f = FilReader(fil_file)
     nchan = f.header['nchans']
     fch1 = f.header['fch1']
